30cc29bfa56a3a0266fae13996e8ba4772302e35
KennetTech <64315533+KennetTech@users.noreply.github.com>
2024-11-25 22:54:10 +0100

    Project - Chapter 1

---
+++ Chapter1/project/Entrance.java
@@ -0,0 +1,11 @@
+package Chapter1.project;
+
+public class Entrance {
+    public static void main(String[] args) {
+        System.out.println("Welcome, to Mesozoic Eden.");
+        String openingHours = "08:00";
+        String closingHours = "20:00";
+        System.out.println("The parks opening hours are from " + openingHours + " to " + closingHours + ".");
+        System.out.println("Mesozoic Eden is safe and secure.");
+    }
+}
