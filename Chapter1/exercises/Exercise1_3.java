1ab25d3fcc4c7b4e86d7528674779072919a577e
KennetTech <64315533+KennetTech@users.noreply.github.com>
2024-11-25 22:38:43 +0100

    Exercise 3 - Chapter 1

---
+++ Chapter1/exercises/Exercise1_3.java
@@ -0,0 +1,10 @@
+package Chapter1.exercises;
+
+public class Exercise1_3 {
+    public static void main(String[] args) {
+        String openingHours = "08:00";
+        String closingHours = "20:00";
+
+        System.out.println("The parks opening hours are from " + openingHours + " to " + closingHours + ".");
+    }
+}
