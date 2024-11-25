41c2ab15cbb86cc79ba2787ee18a25266e0ed628
KennetTech <64315533+KennetTech@users.noreply.github.com>
2024-11-25 22:33:16 +0100

    Chapter 1 - Exercise 2

---
+++ Chapter1/exercises/Exercise1_2.java
@@ -0,0 +1,10 @@
+package Chapter1.exercises;
+
+public class Exercise1_2 {
+    public static void main(String[] args) {
+        String name = "Kennettech";
+        String position = "Park Manager";
+        System.out.println("My name is " + name + 
+        " and I want to ba a " + position + " in Mesozoic Eden.");
+    }
+}
