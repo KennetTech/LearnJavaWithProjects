da3d69c804c1d8a65c2bb17698e08c93fc4a723c
KennetTech <64315533+KennetTech@users.noreply.github.com>
2024-11-25 22:53:23 +0100

    Exercise 5 - Chapter 1

---
+++ Chapter1/exercises/Exercise1_5.java
@@ -0,0 +1,9 @@
+package Chapter1.exercises;
+
+public class Exercise1_5 {
+    public static void main(String[] args) {
+        String name = "KennetTech";
+        System.out.println("Welcome, " + name + " to Mesozoic Eden!");
+    }
+
+}
