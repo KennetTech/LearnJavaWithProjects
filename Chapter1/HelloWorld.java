091449014f34b8ad0a37748d450cb3b87a03731c
KennetTech <64315533+KennetTech@users.noreply.github.com>
2024-11-25 22:16:34 +0100

    Debug exercise - chapter 1

--- Chapter1/HelloWorld.java
+++ Chapter1/HelloWorld.java
@@ -2,6 +2,16 @@ package Chapter1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String greeting = "Hello, World!";
+        System.out.println(greeting);
+
+        int number = 5;
+        int doubled = doubleNumber(number);
+        System.out.println("hte doubled number is:  " +
+        doubled);
+    }
+
+    public static int doubleNumber(int input) {
+        return input * 2; 
     }
 }
