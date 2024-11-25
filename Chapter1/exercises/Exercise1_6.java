4ad53e7934fe9ed2a1e887bd6b9bc62503678f88
KennetTech <64315533+KennetTech@users.noreply.github.com>
2024-11-25 22:53:52 +0100

    Exercise 6 - Chapter 1

---
+++ Chapter1/exercises/Exercise1_6.java
@@ -0,0 +1,10 @@
+package Chapter1.exercises;
+
+public class Exercise1_6 {
+    public static void main(String[] args) {
+        String name = "KennetTech";
+        System.out.println("Welcome, " + name + " to Mesozoic Eden!");
+        System.out.println("Mesozoic Eden is safe and secure.");
+    }
+
+}
